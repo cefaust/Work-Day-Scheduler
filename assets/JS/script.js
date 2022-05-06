9ad61b262e8f187912973f42523867eb9d687d69
Chris Faust <cefaust82@gmail.com>
2022-05-05 18:03:36 -0700

    first commit

---
+++ assets/JS/script.js
@@ -0,0 +1,42 @@
+var saveBtnEl = $(".btn");
+let textareaEls = $("textarea");
+
+setInterval(runEverySecond, 1000);
+function runEverySecond(time) {
+  var unix = moment().format("X");
+  var time = moment(unix, "X").format("MMM DD, YYYY [at] h:mm:ss ");
+  const currentHour = moment(unix, "X").format("H");
+  $("#currentDay").text(time);
+  for (let i = 0; i < textareaEls.length; i++) {
+    const textarea = $(textareaEls[i]);
+    const rowHour = i + 8;
+    if (currentHour < rowHour) {
+      // if in future
+      textarea.css("background-color", "green");
+    } else if (currentHour > rowHour) {
+      // if in past
+      textarea.css("background-color", "grey");
+    } else if (currentHour === rowHour) {
+      // current hour
+      textarea.css("background-color", "red");
+    }
+  }
+}
+runEverySecond()
+
+saveBtnEl.click(function (event) {
+  let textarea = $(event.currentTarget).siblings("textarea");
+  console.log(textarea, textarea.attr("id"));
+  let id = textarea.attr("id");
+  let val = textarea.val();
+
+  localStorage.setItem(id, val);
+});
+
+for (let i = 0; i < textareaEls.length; i++) {
+  const textarea = $(textareaEls[i]);
+  let id = textarea.attr("id");
+
+  let valueFromStorage = localStorage.getItem(id);
+  textarea.val(valueFromStorage);
+}
